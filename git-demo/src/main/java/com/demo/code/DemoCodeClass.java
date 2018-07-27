6695d8ca9379394be507fa3dd523c62ea5cb3640
583629897 <583629897@qq.com>
2018-07-27 20:44:29 +0800

    第一次提交,源码

---
+++ git-demo/src/main/java/com/demo/code/DemoCodeClass.java
@@ -0,0 +1,23 @@
+/*
+ * <p>Copyright: Copyright(C) 2017-2018</p >
+ * <p> 公司：北京新媒传信科技有限公司</p >
+ * FileName: ${file_name}
+ * Description: xxxxx
+ * History:
+ * 版本号    作者    日期    操作
+ * 1.0    ${user} ${date}   x
+ * ...
+ */
+package com.demo.code;
+
+/**
+ * @author wanghang
+ * @version 1.0
+ * @date 2018/7/27 0027下午 20:21
+ */
+public class DemoCodeClass {
+
+    public static void main(String[] args){
+        System.out.println("git 操作练习!!!");
+    }
+}
