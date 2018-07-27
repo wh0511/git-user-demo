aa1dba48eea4e168cdb979f286edd1aaba948018
583629897 <583629897@qq.com>
2018-07-27 21:35:04 +0800

    这是非命令行提交

---
+++ git-demo/src/main/java/com/demo/code/Demo2.java
@@ -0,0 +1,22 @@
+/*
+ * <p>Copyright: Copyright(C) 2017-2018</p >
+ * <p> 公司：北京新媒传信科技有限公司</p >
+ * FileName: ${file_name}
+ * Description: xxxxx
+ * History:
+ * 版本号    作者    日期    操作
+ * 1.0    ${user} ${date}   x
+ * ...
+ */
+package com.demo.code;
+
+/**
+ * @author wanghang
+ * @version 1.0
+ * @date 2018/7/27 0027下午 21:31
+ */
+public class Demo2 {
+    public static void main(String[] args){
+        System.out.println("haha !!!");
+    }
+}
